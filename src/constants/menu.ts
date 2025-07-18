09a04cb7451c8b2a6f36e96b3664416a5788599c
drkevinling <dr.kevinling@gmail.com>
2025-07-19 02:58:51 +0630

    zodiac menu July 2025 update

--- src/constants/menu.ts
+++ src/constants/menu.ts
@@ -1,7 +1,7 @@
 const KITCHEN_1 =
-  'https://mcvqsddwagugbgswsepk.supabase.co/storage/v1/object/public/zodiac-menu/zodiac-menu/kitchen-1.jpeg';
+  'https://mcvqsddwagugbgswsepk.supabase.co/storage/v1/object/public/zodiac-menu/zodiac-menu/kitchen--1.jpeg';
 const KITCHEN_2 =
-  'https://mcvqsddwagugbgswsepk.supabase.co/storage/v1/object/public/zodiac-menu/zodiac-menu/kitchen-2.jpeg';
+  'https://mcvqsddwagugbgswsepk.supabase.co/storage/v1/object/public/zodiac-menu/zodiac-menu/kitchen--2.jpeg';
 
 const COCKTAIL_1 =
   'https://mcvqsddwagugbgswsepk.supabase.co/storage/v1/object/public/zodiac-menu/zodiac-menu/cocktail-1.jpeg';
@@ -10,22 +10,49 @@ const COCKTAIL_2 =
   'https://mcvqsddwagugbgswsepk.supabase.co/storage/v1/object/public/zodiac-menu/zodiac-menu/cocktail-2.jpeg';
 
 const FRESH_JUICE =
-  'https://mcvqsddwagugbgswsepk.supabase.co/storage/v1/object/public/zodiac-menu/zodiac-menu/fresh-juice.jpeg';
+  'https://mcvqsddwagugbgswsepk.supabase.co/storage/v1/object/public/zodiac-menu/zodiac-menu/fresh-fruit-juice.jpeg';
 
 const BAKERY =
-  'https://mcvqsddwagugbgswsepk.supabase.co/storage/v1/object/public/zodiac-menu/zodiac-menu/bakery-menu.jpeg';
+  'https://mcvqsddwagugbgswsepk.supabase.co/storage/v1/object/public/zodiac-menu/zodiac-menu/bakery.jpeg';
 
 const COFFEE_1 =
-  'https://mcvqsddwagugbgswsepk.supabase.co/storage/v1/object/public/zodiac-menu/zodiac-menu/drink_1.jpeg?t=2024-09-26T10%3A13%3A37.663Z';
+  'https://mcvqsddwagugbgswsepk.supabase.co/storage/v1/object/public/zodiac-menu/zodiac-menu/drink-1.jpeg';
 
 const COFFEE_2 =
-  'https://mcvqsddwagugbgswsepk.supabase.co/storage/v1/object/public/zodiac-menu/zodiac-menu/drink_2.jpeg?t=2024-09-26T10%3A14%3A09.965Z';
+  'https://mcvqsddwagugbgswsepk.supabase.co/storage/v1/object/public/zodiac-menu/zodiac-menu/drink-2.jpeg';
 
 const LOGO =
   'https://mcvqsddwagugbgswsepk.supabase.co/storage/v1/object/public/zodiac-menu/zodiac-menu/logo.jpg?t=2024-09-26T10%3A17%3A42.408Z';
 
 const ADDRESS =
   'https://mcvqsddwagugbgswsepk.supabase.co/storage/v1/object/public/zodiac-menu/zodiac-menu/address.jpeg?t=2024-10-14T14%3A23%3A39.839Z&v=1';
+
+const ASIAN_CUISINE =
+  'https://mcvqsddwagugbgswsepk.supabase.co/storage/v1/object/public/zodiac-menu/zodiac-menu/asian-cuisine.jpeg';
+
+const CHEESE_CAKE =
+  'https://mcvqsddwagugbgswsepk.supabase.co/storage/v1/object/public/zodiac-menu/zodiac-menu/cheese-cake.jpeg';
+
+const FALOODA =
+  'https://mcvqsddwagugbgswsepk.supabase.co/storage/v1/object/public/zodiac-menu/zodiac-menu/falooda.jpeg';
+
+const JAPAN_MATCHA =
+  'https://mcvqsddwagugbgswsepk.supabase.co/storage/v1/object/public/zodiac-menu/zodiac-menu/japan-matcha.jpeg';
+
+const KOREAN_CUISINE =
+  'https://mcvqsddwagugbgswsepk.supabase.co/storage/v1/object/public/zodiac-menu/zodiac-menu/korean-cuisine.jpeg';
+
+const PANDAN_CAKE =
+  'https://mcvqsddwagugbgswsepk.supabase.co/storage/v1/object/public/zodiac-menu/zodiac-menu/pandan-cake.jpeg';
+
+const TOM_YUM_NOODLE =
+  'https://mcvqsddwagugbgswsepk.supabase.co/storage/v1/object/public/zodiac-menu/zodiac-menu/tom-yum-noodle.jpeg';
+
+const YOGURT =
+  'https://mcvqsddwagugbgswsepk.supabase.co/storage/v1/object/public/zodiac-menu/zodiac-menu/yogurt.jpeg';
+
+const ZODIAC_BLISS =
+  'https://mcvqsddwagugbgswsepk.supabase.co/storage/v1/object/public/zodiac-menu/zodiac-menu/zodiac-bliss.jpeg';
 
 export {
   LOGO,
@@ -38,4 +65,13 @@ export {
   BAKERY,
   COFFEE_1,
   COFFEE_2,
+  ASIAN_CUISINE,
+  CHEESE_CAKE,
+  FALOODA,
+  JAPAN_MATCHA,
+  KOREAN_CUISINE,
+  PANDAN_CAKE,
+  TOM_YUM_NOODLE,
+  YOGURT,
+  ZODIAC_BLISS,
 };
