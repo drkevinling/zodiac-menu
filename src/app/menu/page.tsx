ce2318f7b42c3ca259b8365be7444809f17e3093
drkevinling <dr.kevinling@gmail.com>
2025-07-18 19:56:47 +0630

    zodiac coffee lounge

--- src/app/menu/page.tsx
+++ src/app/menu/page.tsx
@@ -31,7 +31,7 @@ export default function Menu() {
         <MenuItem pic={LOGO} />
       </div>
       <div className=" flex text-2xl font-bold justify-center w-full  p-5">
-        <p>Welcome to Zodiac Cafe & Bar </p>
+        <p>Welcome to Zodiac Coffee Lounge</p>
       </div>
 
       <div className="h-screen flex flex-col">
