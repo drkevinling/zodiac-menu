5d42af19d90ff57d3b2d11632c494a9f3c7fe567
drkevinling <dr.kevinling@gmail.com>
2025-07-21 20:27:03 +0630

    root page update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,51 +1,62 @@
-import Image from 'next/image';
+'use client';
+import MenuItem from '@/components/menu';
+import {
+  KITCHEN_1,
+  KITCHEN_2,
+  COCKTAIL_1,
+  COCKTAIL_2,
+  FRESH_JUICE,
+  BAKERY,
+  COFFEE_1,
+  COFFEE_2,
+  LOGO,
+  ADDRESS,
+  ASIAN_CUISINE,
+  CHEESE_CAKE,
+  FALOODA,
+  JAPAN_MATCHA,
+  KOREAN_CUISINE,
+  PANDAN_CAKE,
+  TOM_YUM_NOODLE,
+  YOGURT,
+  ZODIAC_BLISS,
+} from '@/constants/menu';
 
 export default function Home() {
+  const ListOfMenuPics = [
+    ADDRESS,
+    COFFEE_1,
+    COFFEE_2,
+    FALOODA,
+    JAPAN_MATCHA,
+    FRESH_JUICE,
+    ZODIAC_BLISS,
+    YOGURT,
+    BAKERY,
+    PANDAN_CAKE,
+    CHEESE_CAKE,
+    ASIAN_CUISINE,
+    KOREAN_CUISINE,
+    TOM_YUM_NOODLE,
+    KITCHEN_1,
+    KITCHEN_2,
+    COCKTAIL_1,
+    COCKTAIL_2,
+  ];
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <div className="flex flex-col gap-8 overflow-y-auto ">
-        {/* <KitchenMenu /> */}
+    <>
+      <div className="my-5">
+        <MenuItem pic={LOGO} />
       </div>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <Image
-          aria-hidden
-          src="https://nextjs.org/icons/file.svg"
-          alt="File icon"
-          width={16}
-          height={16}
-        />
-        Learn
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://mcvqsddwagugbgswsepk.supabase.co/storage/v1/object/public/zodiac-menu/zodiac-menu/bakery-menu.jpeg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+      <div className=" flex text-2xl font-bold justify-center w-full  p-5">
+        <p>Welcome to Zodiac Coffee Lounge</p>
+      </div>
+
+      <div className="h-screen flex flex-col">
+        {ListOfMenuPics.map((pic) => (
+          <MenuItem key={pic} pic={pic} />
+        ))}
+      </div>
+    </>
   );
 }
