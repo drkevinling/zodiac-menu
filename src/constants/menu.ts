a8ed0b18ade39b6a7e1502eb58f85c2fbcedf2c8
drkevinling <dr.kevinling@gmail.com>
2025-08-18 15:56:58 +0630

    logo updated

--- src/constants/menu.ts
+++ src/constants/menu.ts
@@ -22,10 +22,10 @@ const COFFEE_2 =
   'https://fhciiwrknsztdrfyodpz.supabase.co/storage/v1/object/public/zodiac-menu/2.drink-2.jpeg';
 
 const LOGO =
-  'https://mcvqsddwagugbgswsepk.supabase.co/storage/v1/object/public/zodiac-menu/zodiac-menu/logo.jpg?t=2024-09-26T10%3A17%3A42.408Z';
+  'https://fhciiwrknsztdrfyodpz.supabase.co/storage/v1/object/public/zodiac-menu/logo.jpg';
 
 const ADDRESS =
-  'https://mcvqsddwagugbgswsepk.supabase.co/storage/v1/object/public/zodiac-menu/zodiac-menu/address.jpeg?t=2024-10-14T14%3A23%3A39.839Z&v=1';
+  'https://fhciiwrknsztdrfyodpz.supabase.co/storage/v1/object/public/zodiac-menu/address.jpeg';
 
 const ASIAN_CUISINE =
   'https://fhciiwrknsztdrfyodpz.supabase.co/storage/v1/object/public/zodiac-menu/13.asian-cuisine.jpeg';
